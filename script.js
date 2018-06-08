bc342862df19d8c42083131cbd89e7433299ec5c
justendekuiper <justen.dekuiper@airbnb.com>
2018-06-08 16:12:10 +0100

    Update

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
+/*Inserted textfield expander */
+
+$(document).ready(function () {
+  $('#EntreeField').each(function () {
+    h(this);
+  }).on('lookup change keyup', function () {
+    h(this);
+  });
+
+
+  function h(e) {
+    $(e).css({'height':'auto','overflow-y':'scroll'}).height(e.scrollHeight);
+  }
+});
+
+
+
+
+
+
+
+
+/*changing inserted text
 function transformtext() {
 	var insertedtext = document.getElementById("EntreeField").value;
 	document.getElementById("DivOutputField").innerText = insertedtext;
 
 }
+*/
 
+function transformtext() {
+	var insertedtext = document.getElementById("EntreeField").value;
+  var cancel_policy = insertedtext.substring(insertedtext.indexOf("cancel_policy") + 15,insertedtext.indexOf("canceled_at")-1)
+
+  if (cancel_policy="9") {
+    cancel_policy= "Super Strict 60";
+  }
+  else {
+    cancel_policy="other";
+  };
+  
+	document.getElementById("cancel_policy").innerText = cancel_policy;
+
+}
